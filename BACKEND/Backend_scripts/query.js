eb5d482e9e92f5f9b39b1d8427dab1c613629ab8
gdanag <garen.danageozian@gmail.com>
2023-01-20 19:19:58 +0400

    adjust filling function parameters and require filling callbacks

--- BACKEND/Backend_scripts/query.js
+++ BACKEND/Backend_scripts/query.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql')
+const insertionCallback = require('./insertMethods')
+const fs = require('fs')
 
 require('dotenv').config({ path: '/home/gdanag/Documents/Sourcemind/Dollarfish_app/BACKEND/Backend_scripts/.env' })
 const { HOST, CURRENT_USER, PASSWORD, DATABASE } = process.env;
@@ -23,16 +25,32 @@ class Database {
         this.fillData = this.fillData.bind(this)
     }
 
-    fillData(myDataName, insertionCallback) {
+    fillData(myDataName, callbackName) {
         const myData = this[myDataName]
+        let myCallback = insertionCallback(callbackName)
+        myCallback = myCallback.bind(this)
         
         for(let i = 0; i < myData.length; i++) {
             try {
-                insertionCallback(myData[i])
+                myCallback(myData[i])
             } catch (error) {
                 console.warn("insertion problem detected")
             }
         }
+    }
+
+    selectSchema() {
+        this.connection.query(`USE ${DATABASE}`)
+    }
+
+    dropTables(tableNames) {
+        for(let i = 0; i < tableNames.length; i++)
+            this.connection.query(`DROP TABLE IF EXISTS ${tableNames[i]};`)
+    }
+
+    createSchemaTables() {
+        let tableCreateQuery = fs.readFileSync('/home/gdanag/Documents/Sourcemind/Dollarfish_app/BACKEND/data/Dollarfish_model.sql').toString();
+        this.connection.query(tableCreateQuery)
     }
 }
 
